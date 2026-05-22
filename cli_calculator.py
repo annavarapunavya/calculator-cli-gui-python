73c07e5384121fddf8dbf579c1b7b0f7f513787c
Navya <annavarapunavya2005@gmail.com>
2026-05-22 18:34:12 +0530

    Added loop and exit functionality to CLI calculator

--- cli_calculator.py
+++ cli_calculator.py
@@ -1,36 +1,44 @@
 #Importing functions from utility file
 from utils import add, subtract, multiply, divide
 
-#Menu to select the Operation
-print("Calculator Menu")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+while(True):
+    #Menu to select the Operation   
+    print("Calculator Menu")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5.Exit")
 
 
-choice = input("Enter your choice (1-4): ")
+    choice = input("Enter your choice (1-4): ")
 
-#Taking input numbers from users
-try:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    #Exit option
+    if choice == "5":
+        print("Exiting calculator...")
+        break
 
-except ValueError:
-    print("Invalid input! Please enter numbers.")
-    exit()
+    #Taking input numbers from users
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
-if choice == "1":
-    print("Result:", add(num1, num2))
+    except ValueError:
+        print("Invalid input! Please enter numbers.")
+        exit()
 
-elif choice == "2":
-    print("Result:", subtract(num1, num2))
+    #Operations
+    if choice == "1":
+        print("Result:", add(num1, num2))
 
-elif choice == "3":
-    print("Result:", multiply(num1, num2))
+    elif choice == "2":
+        print("Result:", subtract(num1, num2))
 
-elif choice == "4":
-    print("Result:", divide(num1, num2))
+    elif choice == "3":
+        print("Result:", multiply(num1, num2))
 
-else:
-    print("Invalid choice")
+    elif choice == "4":
+        print("Result:", divide(num1, num2))
+
+    else:
+        print("Invalid choice")
