ad445667817ba9f274ee05c3cfdfba704690ab80
Navya <annavarapunavya2005@gmail.com>
2026-05-22 18:21:27 +0530

    Completed basic CLI calculator

--- cli_calculator.py
+++ cli_calculator.py
@@ -1,5 +1,7 @@
+#Importing functions from utility file
 from utils import add, subtract, multiply, divide
 
+#Menu to select the Operation
 print("Calculator Menu")
 print("1. Add")
 print("2. Subtract")
@@ -7,3 +9,24 @@ print("3. Multiply")
 print("4. Divide")
 
 
+choice = input("Enter your choice (1-4): ")
+
+#Taking input numbers from users
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == "1":
+    print("Result:", add(num1, num2))
+
+elif choice == "2":
+    print("Result:", subtract(num1, num2))
+
+elif choice == "3":
+    print("Result:", multiply(num1, num2))
+
+elif choice == "4":
+    print("Result:", divide(num1, num2))
+
+else:
+    print("Invalid choice")
