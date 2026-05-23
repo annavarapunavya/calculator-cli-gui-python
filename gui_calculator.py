35a5a6b1ee932690243d0f17f58e99e24e4dded1
Navya <annavarapunavya2005@gmail.com>
2026-05-23 15:41:05 +0530

    Added display screen to GUI calculator

--- gui_calculator.py
+++ gui_calculator.py
@@ -9,5 +9,10 @@ root.title("Calculator")
 # Window size
 root.geometry("300x400")
 
+#Display screen
+display = tk.Entry(root, width=25, font=("Arial", 20), borderwidth=5)
+
+display.pack(pady=20)
+
 # Run application
 root.mainloop()
