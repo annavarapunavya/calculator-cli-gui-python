c9a544510c9952db20ec90ec0340455f348a32bb
Navya <annavarapunavya2005@gmail.com>
2026-06-06 14:36:57 +0530

    Added calculator button functionality

--- gui_calculator.py
+++ gui_calculator.py
@@ -13,16 +13,56 @@ root.geometry("300x400")
 display = tk.Entry(root, width=25, font=("Arial", 20), borderwidth=5)
 display.pack(pady=20)
 
+def button_click(value):
+
+    current = display.get()
+
+    if value == "C":
+        display.delete(0, tk.END)
+
+    elif value == "=":
+        try:
+            result = eval(current)
+            display.delete(0, tk.END)
+            display.insert(0, result)
+
+        except:
+            display.delete(0, tk.END)
+            display.insert(0, "Error")
+
+    else:
+        display.insert(tk.END, value)
+
 #Button Frame
 button_frame = tk.Frame(root)
 button_frame.pack()
 #Number buttons
-button1 = tk.Button(button_frame, text="1", width=5, height =2)
-button2 = tk.Button(button_frame, text="2", width=5, height =2)
-button3 = tk.Button(button_frame, text="3", width=5, height =2)
+buttons = [
+    "7", "8", "9", "/",
+    "4", "5", "6", "*",
+    "1", "2", "3", "-",
+    "0", "C", "=", "+"
+]
 
-button1.grid(row=0, column=0)
-button2.grid(row=0, column=1)
-button3.grid(row=0, column=2)
+row = 0
+col = 0
+
+for button in buttons:
+    btn = tk.Button(
+    button_frame,
+    text=button,
+    width=5,
+    height=2,
+    font=("Arial", 14),
+    command=lambda value=button: button_click(value)
+)
+
+    btn.grid(row=row, column=col, padx=5, pady=5)
+
+    col += 1
+
+    if col > 3:
+        col = 0
+        row += 1
 # Run application
 root.mainloop()
